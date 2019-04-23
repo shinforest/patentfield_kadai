deac4760e2f4659b084d841787e0f25cc89d5c43
tkgsn <shun.0721@hotmail.co.jp>
2019-04-23 05:44:01 +0000

    remove pass, indent, vagrant

--- app/controllers/search_record_controller.rb
+++ app/controllers/search_record_controller.rb
@@ -1,15 +1,15 @@
 class SearchRecordController < ApplicationController
-    protect_from_forgery except: :search
+  protect_from_forgery except: :search
 
-    def index
-        @search_records = SearchRecord.order(:id)
-    end
+  def index
+    @search_records = SearchRecord.order(:id)
+  end
 
-    def search
-        @search_record = SearchRecord.new({value: params[:word]})
-        if  @search_record.save
-          result = Peds::Client.search_by(firstNamedApplicant: params[:word])
-          @docs = result[:body][:queryResults][:searchResponse][:response][:docs]
-        end
-    end        
+  def search
+    @search_record = SearchRecord.new({value: params[:word]})
+      if  @search_record.save
+        result = Peds::Client.search_by(firstNamedApplicant: params[:word])
+        @docs = result[:body][:queryResults][:searchResponse][:response][:docs]
+      end
+  end        
 end
