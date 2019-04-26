391d34cf7433f6f73598ed0032611e78f89d7434
tkgsn <shun.0721@hotmail.co.jp>
2019-04-26 14:42:07 +0000

    change search_record_controller to search_records_controller and so on.

--- app/controllers/search_records_controller.rb
+++ app/controllers/search_records_controller.rb
@@ -1,4 +1,4 @@
-class SearchRecordController < ApplicationController
+class SearchRecordsController < ApplicationController
   protect_from_forgery except: :search
 
   def index
@@ -7,7 +7,7 @@ class SearchRecordController < ApplicationController
 
   def search
     @search_record = SearchRecord.new({value: params[:word]})
-      if  @search_record.save
+      if @search_record.save
         result = Peds::Client.search_by(firstNamedApplicant: params[:word])
         @docs = result[:body][:queryResults][:searchResponse][:response][:docs]
       end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
       confirmations: "users/confirmations"
     }
     get "peds", to: 'peds#index'
-    post "search_record", to: 'search_record#search'
-    resources :search_record
+    post "search_records", to: 'search_records#search'
+    resources :search_records
   end
 end
